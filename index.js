da19d570de13ecfc97e8cb0db7bf8512047e56d3
unknown <khhan1990@hanmail.net>
2022-01-03 21:19:56 +0900

    2022년 1월 3일

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const port = 5000;
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 
 // key에서 dev 및 prod 를 가져오기 위함
 const config = require("./config/key");
@@ -17,6 +18,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // application/json 타입으로 된 것을 분석해서 가져올 수 있게 함
 app.use(bodyParser.json());
+// cookieparser 사용
+app.use(cookieParser());
 
 const mongoose = require("mongoose");
 mongoose
@@ -43,6 +46,40 @@ app.post("/register", (req, res) => {
         // 성공했을 때 메세지 전달
         return res.status(200).json({
             success: true,
+        });
+    });
+});
+
+app.post("/login", (req, res) => {
+    // 요청된 이메일을 데이터베이스에서 있는지 찾는다.
+    User.findOne({ email: req.body.email }, (err, user) => {
+        // 만약 User 컬렉션 안에 제공된 email이 하나도 없다면~
+        if (!user) {
+            return res.json({
+                loginSuccess: false,
+                message: "제공된 이메일에 해당하는 유저가 없습니다.",
+            });
+        }
+
+        // 요청된 이메일이 DB에 있다면 비밀번호가 맞는지 확인
+        // req.body.password는 plainPassword
+        user.comparePassword(req.body.password, (err, isMatch) => {
+            if (!isMatch)
+                return res.json({
+                    loginSuccess: false,
+                    message: "비밀번호가 틀렸습니다.",
+                });
+
+            // 비밀번호까지 맞다면 Token 생성(jsonwebtoken 다운받기)
+            // generateToken 부분은 이름 아무렇게나 작성 가능
+            user.generateToken((err, user) => {
+                if (err) return res.status(400).send(err);
+
+                // token을 저장한다 (cookieParser 인스톨)
+                res.cookie("x_auth", user.token)
+                    .status(200)
+                    .json({ loginSuccess: true, userId: user._id });
+            });
         });
     });
 });
--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+const jwt = require("jsonwebtoken");
 
 const userSchema = mongoose.Schema({
     name: {
@@ -30,6 +33,61 @@ const userSchema = mongoose.Schema({
         type: Number,
     },
 });
+
+// user model에 user 정보를 저장하기 전에 function을 한다는 의미
+userSchema.pre("save", function (next) {
+    var user = this;
+
+    // password가 변화될 때만 bcrypt를 이용해서 암호화한다
+    if (user.isModified("password")) {
+        // 비밀번호를 암호화 시킨다.
+        bcrypt.genSalt(saltRounds, function (err, salt) {
+            // 에러가 났다면 next 로 보냄
+            if (err) return next(err);
+            // salt가 제대로 생성되었다면
+            bcrypt.hash(user.password, salt, function (err, hash) {
+                if (err) return next(err);
+                // 성공했다면 plain password를 hash 비밀번호로 바꿔줌
+                user.password = hash;
+                next();
+            });
+        });
+        // 비밀번호를 바꾸는 것을 바꿀 때는 next()를 해줘야
+        // 바로  register router로 갈 수 있다.
+    } else {
+        next();
+    }
+});
+
+// login router에서 user.comparePassword 부분
+userSchema.methods.comparePassword = function (plainPassword, cb) {
+    // this.password는 암호화 된 비밀번호
+    // plainPassword와 암호화 된 비밀번호가 일치하는지
+    // bcrypt의 compare method를 통해 확인
+    bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+        // 강의에서 cb(err), 로 나오니 cb(err);로 바꿀 것
+        if (err) return cb(err);
+        // 다음에 오는 cb는 err는 null 이고 두 비밀번호는
+        // '같다'라는 뜻
+        cb(null, isMatch);
+    });
+};
+
+// login router에서 user.generateToken 부분
+userSchema.methods.generateToken = function (cb) {
+    var user = this;
+
+    // jsonwebtoken을 이용하여 token 생성하기
+    // user_id + secretToken = token
+
+    var token = jwt.sign(user._id.toHexString(), "secretToken");
+
+    user.token = token;
+    user.save(function (err, user) {
+        if (err) return cb(err);
+        cb(null, user);
+    });
+};
 
 const User = mongoose.model("User", userSchema);
 
